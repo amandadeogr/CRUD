184ed3b70924cf45c61e64dcea63153f504abe7d
amandadeog <amandadeog@gmail.com>
2022-10-13 13:48:48 -0300

    localStorage inclusion

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const closeModal = () => {
    modal.classList.remove('active');
 }
 
+//localstorage
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("db_client")) ?? [];
+
+const setLocalStorage = (dbClient) =>
+  localStorage.setItem("db_client", JSON.stringify(dbClient));
+
 //events
 const registerClientButton = document.getElementById('registerClient');
 registerClientButton.addEventListener('click', openModal);
