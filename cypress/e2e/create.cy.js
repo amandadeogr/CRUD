7b73eb4757869f4ba008c653d090bef32fcfa045
amandadeog <amandadeog@gmail.com>
2022-10-16 22:26:26 -0300

    test the create function to the project

---
+++ cypress/e2e/create.cy.js
@@ -0,0 +1,23 @@
+describe("The user will be create a client", () => {
+  it("Should be visit application", () => {
+    cy.visit("https://crud-simbiose.vercel.app/");
+  });
+
+  it("Should be confirm the application", () => {
+    cy.get("#registerClient").should("be.visible").click();
+    cy.get("#name").should("be.visible");
+    cy.get("#email").should("be.visible");
+    cy.get("#birthDate").should("be.visible");
+  });
+
+  it("Should be create the first client", () => {
+    cy.get("#closeModal").should("be.visible");
+    cy.get("#saveClient").should("be.visible");
+    cy.get("#name").type("Amanda").should("have.value", "Amanda");
+    cy.get("#email")
+      .type("amanda@email.com")
+      .should("have.value", "amanda@email.com");
+    cy.get("#birthDate").type("1998-12-27").should("have.value", "1998-12-27");
+    cy.get("#saveClient").click();
+  });
+});
