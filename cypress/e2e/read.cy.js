7452327058d117846f97da193a652177a0cfb703
amandadeog <amandadeog@gmail.com>
2022-10-16 22:26:53 -0300

    test the read function to the project

---
+++ cypress/e2e/read.cy.js
@@ -0,0 +1,29 @@
+describe("The user should be able to read the client was created", () => {
+  it("visit application", () => {
+    cy.visit("https://crud-simbiose.vercel.app/");
+  });
+
+  it("Should be confirm the application", () => {
+    cy.get("#registerClient").should("be.visible").click();
+    cy.get("#name").should("be.visible");
+    cy.get("#email").should("be.visible");
+    cy.get("#birthDate").should("be.visible");
+  });
+
+  it("Should be create the first client", () => {
+    cy.get("#closeModal").should("be.visible");
+    cy.get("#saveClient").should("be.visible");
+    cy.get("#name").type("Amanda").should("have.value", "Amanda");
+    cy.get("#email")
+      .type("amanda@email.com")
+      .should("have.value", "amanda@email.com");
+    cy.get("#birthDate").type("1998-12-27").should("have.value", "1998-12-27");
+    cy.get("#saveClient").click();
+  });
+
+  it("should be confirm the created client", () => {
+    cy.contains("Amanda");
+    cy.contains("amanda@email.com");
+    cy.contains("27/12/1998");
+  });
+});
